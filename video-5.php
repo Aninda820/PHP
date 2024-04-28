7c955b2d0828eb15f4fb7fb9ca64a54dcaaae3f0
Aninda Betal <michaelshroud01@gmail.com>
2024-04-29 00:01:49 +0530

    PHP variables

---
+++ video-5.php
@@ -0,0 +1,18 @@
+<!-- PHP variables -->
+
+<?php
+echo "Php variables" . "<br>";
+echo 'php is not case sensitive'. '<br>';
+echo 'Variables are case sensitive'.'<br>';
+echo 'Php auto detect variables types';
+
+echo '<hr>';
+
+$name = 'Aninda';
+$income = 20000;
+
+echo 'This guy name is ' . $name .'and income is Rs.' .$income ."<br>";
+echo 'This guy name is $name and income is Rs. $income' ."<br>";
+echo "This guy name is $name and income is Rs. $income";
+
+?>
