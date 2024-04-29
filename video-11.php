a217d7a67360f1dd4a1c15ce7a48fb4369d177c0
Aninda Betal <michaelshroud01@gmail.com>
2024-04-29 06:35:43 +0530

    Switch Case Statements in php

---
+++ video-11.php
@@ -0,0 +1,27 @@
+<?php
+$age = 67;
+
+switch($age){
+    case 12:
+        echo "your age is 12 years";
+        break;
+    case 13:
+        echo "your age is 13 years";
+        break;
+    case 14:
+        echo "your age is 14 years";
+        break;
+    case 15:
+        echo "your age is 15 years";
+        break;
+    case 16:
+        echo "your age is 16 years";
+        break;
+    case 17:
+  	    echo "your age is 17 years";
+        break;
+    default: 
+        echo "Invalid input";
+        break;
+    }
+?>
